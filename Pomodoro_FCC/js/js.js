5c6d3efdfb028c79c62346afb06594c56d0fd14c
Mahaveer sharma <Mahaveer sharma>
2017-03-20 12:21:37 +0530

    work and break time selection complete

--- Pomodoro_FCC/js/js.js
+++ Pomodoro_FCC/js/js.js
@@ -1,1 +1,33 @@
+var breakDecrement = document.querySelector('.break-decrement');
+var breakIncrement = document.querySelector('.break-increment');
+var workDecrement = document.querySelector('.work-decrement');
+var workIncrement = document.querySelector('.work-increment');
+var breakTime = document.querySelector('.break').innerHTML;
+var workTime = document.querySelector('.work').innerHTML;
 
+breakDecrement.addEventListener('click', () => {
+   breakTime--;
+   console.log(breakTime);
+   document.querySelector('.break').innerHTML = breakTime
+})
+
+breakIncrement.addEventListener('click', () => {
+   breakTime++;
+   console.log(breakTime);
+   document.querySelector('.break').innerHTML = breakTime
+
+})
+
+workDecrement.addEventListener('click', () => {
+   workTime--;
+   console.log(workTime);
+   document.querySelector('.work').innerHTML = workTime
+
+})
+
+workIncrement.addEventListener('click', () => {
+   workTime++;
+   console.log(workTime);
+   document.querySelector('.work').innerHTML = workTime
+
+})
