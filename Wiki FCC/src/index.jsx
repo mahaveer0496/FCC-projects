687da609086080aff6e25583ee773122ab1daf6c
Mahaveer <mahaveersharma0496@yahoo.com>
2017-05-29 23:03:17 +0530

    added airbnb styleguide

--- Wiki FCC/src/index.jsx
+++ Wiki FCC/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { render } from 'react-dom';
 import './styles/main.scss';
+
 import App from './components/App';
 
 render(
-  <App />,
-  document.getElementById('app'),
+  <App />, document.getElementById('app'),
 );
