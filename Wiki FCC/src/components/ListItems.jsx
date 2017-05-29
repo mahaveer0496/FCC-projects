f86a2cd817e3c55f1337dcfdf6dc97fbbe56af71
Mahaveer <mahaveersharma0496@yahoo.com>
2017-05-30 02:02:51 +0530

    initial listitems commit

--- Wiki FCC/src/components/ListItems.jsx
+++ Wiki FCC/src/components/ListItems.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const ListItems = props => (
-  <div>
-    Hi this is list
+  <div className="list">
+    <p className="list__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt et aut explicabo, nam, magni in a ipsum eaque asperiores assumenda impedit maxime perspiciatis hic voluptatibus quasi aspernatur unde. Quibusdam, ad!
+    </p>
+    <a href="/" className="list__link">Read more...</a>
   </div>
 );
 
