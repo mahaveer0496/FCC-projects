b4e8de82f75b9cd49038780348ad0c0133c148cc
Mahaveer <mahaveersharma0496@yahoo.com>
2017-05-30 19:22:06 +0530

    tiny update

--- Wiki FCC/src/components/ListItem.jsx
+++ Wiki FCC/src/components/ListItem.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const ListItem = ({ title, description, url }) => (
   <li className="list">
     <h3 className="list__title">{title}</h3>
-    <p className="list__text">{description.substr(0, 120)}...</p>
-    <a className="list__link" target="_blank" rel="noopener noreferrer" href={url} >Read more...</a>
+    <p className="list__text">{description.substr(0, 150)}...</p>
+    <a className="list__link" target="_blank" rel="noopener noreferrer" href={url} >Read more</a>
   </li>
 );
 
