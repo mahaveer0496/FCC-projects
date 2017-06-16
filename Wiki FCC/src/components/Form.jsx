27d52795520116437fb384f69d20e7884e908a1e
mahaveer <mahaveersharma0496@yahoo.com>
2017-06-17 00:28:36 +0530

    final commit

--- Wiki FCC/src/components/Form.jsx
+++ Wiki FCC/src/components/Form.jsx
@@ -15,29 +15,27 @@ class Form extends Component {
   }
   render() {
     return (
-      <div>
-        <form className="form" onSubmit={(e) => { e.preventDefault(); }} >
-          <div className="form__search-field">
-            <label className="form__hidden" htmlFor="text">Search</label>
-            <input
-              id="text"
-              type="text"
-              autoFocus
-              placeholder="Start typing"
-              onChange={this.callFetch}
-              required
-            />
-            <a
-              href="http://en.wikipedia.org/wiki/Special:Random"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="button button-pink"
-            >
-              <i className="ion-shuffle" />
-            </a>
-          </div>
-        </form>
-      </div >
+      <div className="form">
+        <div className="form__search-field">
+          <label className="form__hidden" htmlFor="text">Search</label>
+          <input
+            id="text"
+            type="text"
+            autoFocus
+            placeholder="Start typing"
+            onChange={this.callFetch}
+            required
+          />
+          <a
+            href="http://en.wikipedia.org/wiki/Special:Random"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button-pink"
+          >
+            <i className="ion-shuffle" />
+          </a>
+        </div>
+      </div>
     );
   }
 }
